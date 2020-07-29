8f8aed5807b83402dcd4977a667e9e3a6d31a06d
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2020-07-28 23:11:37 -0600

    Data analysis of the first 5 repetitions and simulation files of rep6, 7 of Test 3, System 1

--- System1/Test_3/MetaD_EXE/biasf_50/run.sh
+++ System1/Test_3/MetaD_EXE/biasf_50/run.sh
@@ -1,9 +1,21 @@
-cd rep_2
+cd rep_11
 gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
-cd ../rep_3
+cd ../rep_12
 gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
-cd ../rep_4
+cd ../rep_13
 gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
-cd ../rep_5
+cd ../rep_14
+gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
+cd ../rep_15
+gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
+cd ../rep_16
+gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
+cd ../rep_17
+gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
+cd ../rep_18
+gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
+cd ../rep_19
+gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
+cd ../rep_20
 gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
 cd ../
