c2abefc8065cf3e691dbc4a5338c2152975d0ab7
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2020-08-05 16:24:03 -0600

    Repository clearning - System 1 (status: all results are satisfactory)

--- System1/Test_3/MetaD_EXE/larger_pace/prep.sh
+++
@@ -1,4 +0,0 @@
-for i in {2..20}
-  do 
-      cp -r rep_1 rep_$i
-  done
--- System1/Test_3/MetaD_EXE/larger_pace/run.sh
+++
@@ -1,6 +0,0 @@
-for i in {1..20}
-  do 
-      cd rep_$i
-      gmx mdrun -s sys1.tpr -x sys1.xtc -c sys1_output.gro -e sys1.edr -dhdl sys1_dhdl.xvg -g sys1.log -plumed
-      cd ../
-  done
