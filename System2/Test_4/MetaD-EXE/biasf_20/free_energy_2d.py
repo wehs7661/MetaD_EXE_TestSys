a51f643d0e8f1c4ea470f2ec9a0eb1b3a245f8db
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2020-08-18 19:52:14 -0600

    2D lambda-MetaD of System 2 (bias factor = 20), rep_1 to rep_9

---
+++ System2/Test_4/MetaD-EXE/biasf_20/free_energy_2d.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+fes = [] 
+for i in range(16):
+    f = open(f"rep_{i + 1}/sum_hills_log.txt", 'r')
+    lines = f.readlines()
+    f.close()
+
+    line_n = 0
+    for l in lines:
+        line_n += 1
+        if "Free energy difference averaging" in l:
+            fes.append(float(lines[line_n - 1].split(':')[-1]))
+
+print(fes.index(18.58874738657086))
+fes.remove(18.58874738657086)
+print(max(fes), min(fes)) 
+print(np.mean(fes))
+print(np.std(fes))
