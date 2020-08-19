cb7ff340a876abfece5b48cd60b0c6559880b9b5
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2020-08-18 19:33:03 -0600

    Bias factor being 5 and 10, 1 rep for each, 2D lambda-MetaD for System 2 (result: the height decreased too fast)

---
+++ System2/Test_4/MetaD-EXE/biasf_50/free_energy_2d.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+fes = [] 
+for i in range(6):
+    f = open(f"rep_{i + 1}/sum_hills_log.txt", 'r')
+    lines = f.readlines()
+    f.close()
+
+    line_n = 0
+    for l in lines:
+        line_n += 1
+        if "Free energy difference averaging" in l:
+            fes.append(float(lines[line_n - 1].split(':')[-1]))
+
+print(fes)
+print(max(fes), min(fes)) 
+print(np.mean(fes))
+print(np.std(fes))
