6b537ff0cf0392739d4d0f3e3ac2b13112c5544a
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2020-11-06 15:24:17 -0700

    Some data analysis code for the old simulations (version 0.1.3) in Test 3, System 1

---
+++ System1/Test_3/MetaD_EXE/rep_7/reweight.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+set -e   # exit upon error 
+
+# some default values
+colvar_i=COLVAR
+colvar_o=COLVAR_analysis
+plumed=plumed_analysis.dat
+n_CVs=1
+kbT=2.4777090399459767  # T=298
+
+while getopts "ic:oc:p:x:n:" opt; do
+    case $opt in
+    ic) colvar_i=${OPTARG};;   # the input COLVAR file for the plumed driver
+    oc) colvar_o=${OPTARG};;   # the output COLVAR file for the plumed driver
+    p) plumed=${OPTARG};;      # the input plumed file for the plumed driver
+    x) xtc=${OPTARG};;         # the xtc file for the plumed driver
+    n) n_CVs=${OPTARG};;       # the number of collective variables 
+    kt) kbT=${OPTARG};;        # the conversion factor from kbT to kJ/mol 
+    esac
+done
+
+awk 'NR==1 || NR%100==2' $colvar_i > COLVAR_awk
+plumed driver --plumed $plumed --noatoms
+bmax=`awk 'BEGIN{max=0.}{if($1!="#!" && $3>max)max=$3}END{print max}' $colvar_o`
+awk '{if($1!="#!") print $2,exp(($3-bmax)/kbt)}' kbt=$kbT bmax=$bmax $colvar_o > weights.dat
