bedc2786f6125a81614de9a31fba5d8ba7553fc7
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2020-11-06 15:44:49 -0700

    Additional tests in System2: 2D_lambda_MetaD

---
+++ System2/Additional_tests/2D_lambda_MetaD/hills_analysis.py
@@ -0,0 +1,15 @@
+import numpy as np
+from free_energy_estimator import FreeEnergy_HILLS
+
+sample_cmd = 'plumed sum_hills --idw lambda --min -pi,0 --max pi,8 --bin 50,8 --outfile fes_test --kt 2.4777090399459767'
+
+df = []
+for i in range(1, 2):
+    estimator = FreeEnergy_HILLS(f'rep_{i}/HILLS_2D')
+    df.append(estimator.deltaF_average(sample_cmd, 19000, 20000) / 2.4777090399459767)
+
+print(df)
+print(np.mean(df))
+print(np.std(df))
+
+
---
+++ System2/Additional_tests/2D_lambda_MetaD/rep_1/diffusion_2d.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+rc('font', **{
+    'family': 'sans-serif',
+    'sans-serif': ['DejaVu Sans'],
+    'size': 10
+    })
+
+# Set the font used for MathJax - more on this later
+rc('mathtext', **{'default': 'regular'})
+plt.rc('font', family='serif')
+
+t, theta, Lambda = [], [], []
+infile = open('COLVAR', 'r')
+lines = infile.readlines()
+infile.close()
+
+for line in lines:
+    if line[0] != '#' and line[0] != '@' and len(line.split()) == 4:
+        t.append(float(line.split()[0]))
+        theta.append(float(line.split()[1]))
+        Lambda.append(float(line.split()[2]))
+
+theta = list(np.array(theta) * 180 / np.pi)
+
+plt.figure()
+plt.scatter(theta[::100], Lambda[::100], marker='x', s=2, c=plt.cm.plasma(np.linspace(0, 1, len(theta[::100]))))
+plt.xlabel('Torsional angle relevant to the H.B. (degree)')
+plt.ylabel('Index of the alchemical state')
+plt.grid()
+plt.savefig('diffusion_2d.png', dpi=600)
+
+plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(Lambda, theta, np.array(t) / 1000, c=t, cmap='Greens')
+ax.set_ylabel('Torsional angle relevant to the H.B. (degree)')
+ax.set_xlabel('Index of the alchemical state')
+ax.set_zlabel('Time (ns)')
+plt.savefig('diffusion_3d.png', dpi=600)
+
+
+
