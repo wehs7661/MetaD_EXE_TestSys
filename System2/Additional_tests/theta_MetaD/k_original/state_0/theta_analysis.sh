c338bd483a42265f9c6b82767f59ec12fa580262
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2022-02-03 20:54:38 -0700

    Repository clean-up: System2/Additional_test, lambda_sampling_k_20 and theta_MetaD

--- System2/Additional_tests/theta_MetaD/k_original/state_0/theta_analysis.sh
+++
@@ -1,5 +0,0 @@
-# find maximum value of bias
-bmax=`awk 'BEGIN{max=0.}{if($1!="#!" && $3>max)max=$3}END{print max}' COLVAR`
-
-# print phi values and weights
-awk '{if($1!="#!") print $2,exp(($3-bmax)/kbt)}' kbt=2.4777090399459767 bmax=$bmax COLVAR > theta.weight
