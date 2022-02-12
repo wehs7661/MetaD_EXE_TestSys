4540a8a5f0376ffb735144f5e1b32dcd4d4ee213
wehs7661 <wehs7661@colorado.edu>
2022-02-12 13:25:21 -0500

    Added System3/Test_2/MetaD-EXE/10, 13, 17, 21

---
+++ System3/Test_2/MetaD-EXE/trial_13/backup/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name trial_13
+#SBATCH -p RM
+#SBATCH -N 1
+#SBATCH -t 48:00:00
+#SBATCH --ntasks-per-node=128
+
+source /jet/home/wehs7661/pkgs/gromacs/2020.4/bin/GMXRC
+source /jet/home/wehs7661/src/plumed2/sourceme.sh
+module load gcc/10.2.0
+module load openmpi/3.1.6-gcc10.2.0
+
+mpirun -np 128 gmx_mpi mdrun -s sys3.tpr -x sys3.xtc -c sys3_output.gro -e sys3.edr -dhdl sys3_dhdl.xvg -g sys3.log -plumed -ntomp 1
+
+
