c12686fb4c731af878f6764fd6c3c03d06a37037
wehs7661 <wehs7661@colorado.edu>
2022-02-12 14:15:09 -0500

    Added MetaD_EXE_TestSys/System2/0813_Simulations/4vdw_Sys/charged/additional_exp/Exp_6 to Exp_10

---
+++ System2/0813_Simulations/4vdw_Sys/charged/additional_exp/Exp_9/Group_1/Analysis/plot_validity_tests.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt 
+from matplotlib import rc 
+import numpy as np
+
+if __name__ == "__main__":
+    rc("font", **{"family": "sans-serif", "sans-serif": ["DejaVu Sans"], "size": 10})
+    # Set the font used for MathJax - more on this later
+    rc("mathtext", **{"default": "regular"})
+    plt.rc("font", family="serif")
+
+    # Plot the p-values obtained from the strict tests
+    p_values = []
+    for i in range(5):
+        f = open(f'results_strict_{i}.txt', 'r')
+        lines = f.readlines()
+        f.close()
+
+        data = lines[-1].split('[')[1].split(']')[0].split(',')
+        p_data = [float(data[i]) for i in range(len(data))]
+        p_values.append(p_data)
+    
+    p_values = np.transpose(p_values)
+    labels = ['Total', 'Translational', 'Rot & Int', 'Rotational', 'Internal']
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for i in range(5):
+        plt.plot(range(1, 6), p_values[i], label=labels[i])
+    ax.set_yscale('log')
+    plt.xlabel('Time interval')
+    plt.ylabel('p-values')
+    plt.title('Experimental 9, Group 1')
+    plt.grid()
+    plt.legend()
+    plt.savefig('p_values_results.png', dpi=600)
+
+    # Plot the temperatures obtained from non-strict tests
+    T_mu, T_sigma, T_mu_err, T_sigma_err = [], [], [], []
+    for i in range(5): 
+        T_mu_data, T_sigma_data = [], []
+        T_mu_err_data, T_sigma_err_data = [], []
+        T_ref = 298
+
+        f = open(f'results_non-strict_{i}.txt', 'r')
+        lines = f.readlines()
+        f.close()
+
+        for l in lines:
+            if 'T(mu)' in l:
+                T_mu_data.append(float(l.split('T(mu) = ')[1].split('+-')[0]))
+                T_mu_err_data.append(float(l.split('T(mu) = ')[1].split('+-')[1].split('K')[0]))
+            elif 'T(sigma)' in l:
+                T_sigma_data.append(float(l.split('T(sigma) = ')[1].split('+-')[0]))
+                T_sigma_err_data.append(float(l.split('T(sigma) = ')[1].split('+-')[1].split('K')[0]))
+
+        T_mu.append(T_mu_data)
+        T_sigma.append(T_sigma_data)
+        T_mu_err.append(T_mu_err_data)
+        T_sigma_err.append(T_sigma_err_data)
+
+    T_mu = np.transpose(T_mu)
+    T_sigma = np.transpose(T_sigma)
+    T_mu_err = np.transpose(T_mu_err)
+    T_sigma_err = np.transpose(T_sigma_err)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for i in range(5):
+        plt.errorbar(range(1, 6), T_mu[i], yerr=T_mu_err[i], label=labels[i])
+    plt.xlabel('Time interval')
+    plt.ylabel('T_mu')
+    plt.title('Experimental 9, Group 1')
+    plt.grid()
+    plt.legend()
+    plt.savefig('T_mu_results.png', dpi=600)    
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for i in range(5):
+        plt.errorbar(range(1, 6), T_sigma[i], yerr=T_sigma_err[i], label=labels[i])
+    plt.xlabel('Time interval')
+    plt.ylabel('T_sigma')
+    plt.title('Experimental 9, Group 1')
+    plt.grid()
+    plt.legend()
+    plt.savefig('T_sigma_results.png', dpi=600)    
---
+++ System2/0813_Simulations/4vdw_Sys/charged/additional_exp/Exp_9/Group_7/Analysis/run_validity_tests.py
@@ -0,0 +1,48 @@
+import os
+import glob
+import physical_validation
+import numpy as np
+import time
+import pickle
+import contextlib
+
+if __name__ == "__main__":
+    t1 = time.time()
+    
+    p_values, z_scores = [], []
+    bools = [True, False]
+    test_type = ['strict', 'non-strict']
+    times = np.linspace(0, 25000, 6)
+
+    trr_files = glob.glob('*.trr')
+    if len(trr_files) < 5:
+        for i in range(5):
+            os.system(f'echo 2 | gmx trjconv -s ../sys2_cccc.tpr -f ../sys2_cccc.trr -o sys2_cccc_lig_{i}.trr -b {times[i]} -e {times[i + 1]}')
+
+    for i in range(len(bools)):
+        for j in range(5):
+            print(f'Running {test_type[i]} tests for the trajectory from {times[j]} to {times[j + 1]} ps ...')
+
+            parser = physical_validation.data.GromacsParser()
+
+            res = parser.get_simulation_data(
+            mdp='../mdout.mdp',
+            top='sys2_cccc_lig.top',
+            edr='../sys2_cccc.edr',
+            trr=f'sys2_cccc_lig_{j}.trr'
+            )
+
+            res.system.ndof_reduction_tra = 0
+            
+            with open(f'results_{test_type[i]}_{j}.txt', 'a') as f:
+                with contextlib.redirect_stdout(f):
+                    results = physical_validation.kinetic_energy.equipartition(
+                        data=res,
+                        strict=bools[i],
+                        filename=f'exp9_group3_{test_type[i]}_{j}',
+                    )
+                    f.write(str(results))
+
+    t2 = time.time()
+    print(f'Time elapsed: {t2-t1} seconds.')
+
