4ecd7be97f55079412ddf33521839c38a73c441e
wehs7661 <wehs7661@colorado.edu>
2022-02-12 13:20:42 -0500

    Repository clean-up: System3/Ligand_simulations/EXE/trial_2/

---
+++ System3/Ligand_simulations/EXE/trial_2/Wang-Landau/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name ligand_2
+#SBATCH -p RM
+#SBATCH -N 2
+#SBATCH -t 48:00:00
+#SBATCH --ntasks-per-node=28
+
+source /home/wehs7661/src/plumed2/sourceme.sh
+source /home/wehs7661/pkgs/gromacs/2020.2/bin/GMXRC
+module load gcc/10.1.0
+module load mpi/intel_mpi
+
+# export OMP_NUM_THREADS=24
+
+mpirun -np 56 gmx_mpi mdrun -s sys3_gst.tpr -x sys3_gst.xtc -c sys3_gst_output.gro -e sys3_gst.edr -dhdl sys3_gst_dhdl.xvg -g sys3_gst.log -cpi state.cpt
